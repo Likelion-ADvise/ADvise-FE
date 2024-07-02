d9c44537375803d9bcfd202fa45e36772d98ba59
nayeon park <pny7211@gmail.com>
2024-07-02 14:52:09 +0900

    feat:add auctionlists route and temporarily text

--- src/components/AuctionList.jsx
+++ src/components/AuctionList.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
-
-const WrapGridContiner = styled.div`
+const WrapGridContainer = styled.div`
   max-width: 1080px;
   margin: 0 auto;
 `;
@@ -14,11 +13,13 @@ const GridContainer = styled.div`
   gap: 20px;
   width: 1080px;
   height: 950px;
-  position: absolute;
+  position: relative;
+  top: 50px;
 `;
 
 const FormContainer = styled.div`
   width: 100%;
+  //height: 250px;
   padding-top: 100%;
   position: relative;
   overflow: hidden;
@@ -26,8 +27,6 @@ const FormContainer = styled.div`
   background-color: #f8f9fa;
   box-shadow: inset 0px 0px 0px 1px rgba(0, 0, 0, 0.15);
   box-sizing: border-box;
-  left: 4px;
-  top: 7px;
   cursor: pointer;
 
   &:hover {
@@ -35,27 +34,69 @@ const FormContainer = styled.div`
   }
 `;
 
+const ContentWrapper = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 10px;
+  right: 10px;
+  bottom: 10px;
+  
+`;
+
+const Title = styled.h3`
+  margin: 0;
+  color: #333;
+  font-size: 1.3rem;
+  position: absolute;
+  top: 10px;
+`;
+
+const MinimumPrice = styled.p`
+  margin: 10px 0 0;
+  color: #3CB371;
+  font-size: 1rem;
+  position: absolute;
+  top: 40px;
+`;
+
+const Content = styled.p`
+  margin: 5px 0 0;
+  color: #666;
+  font-size: 0.9rem;
+  position: absolute;
+  top: 100px;
+`;
 
 const AuctionList = () => {
   const navigate = useNavigate();
+  const formItems = [
+    { id: 'id1', title: '', minimum_price: '', content: '' },
+    { id: 'id2', title: '', minimum_price: '', content: '' },
+    { id: 'id3', title: '', minimum_price: '', content: '' },
+    { id: 'id4', title: '', minimum_price: '', content: '' },
+    { id: 'id5', title: '', minimum_price: '', content: '' },
+    { id: 'id6', title: '', minimum_price: '', content: '' },
+    
+  ];
 
-  const handleClick = () => {
-    navigate(`/AuctionDetail`);
+  const handleClick = (id) => {
+    navigate(`/AuctionDetail/${id}`);
   };
 
-  const boxes = Array.from({ length: 6 }, (_, index) => (
-    <FormContainer key={index} onClick={handleClick} />
-  )); //박스 갯수 조정
-
   return (
-    <>
-      <WrapGridContiner>
-        <GridContainer>
-          {boxes}
-         
-        </GridContainer>
-      </WrapGridContiner>
-    </>
+    <WrapGridContainer>
+      <GridContainer>
+        {formItems.map((item) => (
+          <FormContainer key={item.id} onClick={() => handleClick(item.id)}>
+            <ContentWrapper>
+              <Title>제목 : 멋사빌딩 옥외 광고 제안{item.title}</Title>
+              <MinimumPrice>최저 : 999999 {item.minimum_price} ₩</MinimumPrice>
+              <Content>content : 멋쟁이 사자처럼 본사 건물의 옥외 빌딩 광고를 제안합니다.{item.content}</Content>
+              </ContentWrapper>
+          </FormContainer>
+        ))}
+      </GridContainer>
+    </WrapGridContainer>
   );
 };
 
--- src/pages/AuctionLists.jsx
+++ src/pages/AuctionLists.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AuctionList from "../components/AuctionList";
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 
 const Container = styled.div`
   height: 1000px;
 `
 
+
+
+
 const AuctionLists =() => {
   
   const { keyword } = useParams();
+  const[auctionList,setAuctionList]= useState([]);
+ 
+ /* useEffect(()=> {
+    axios
+    .get(``)
+    .then((res)=> {
+      setAuctionList(res.data);
+    })
+    .catch((err)=> {
+      console.log(e);
+    })
+  },[]);*/
 
   
   return(
       <>
-      <div>{keyword}</div>
+      
       <Container>
-      <AuctionList></AuctionList>
+      <AuctionList> </AuctionList>
+      <div>{keyword}</div>
       </Container>
       </>
   );
